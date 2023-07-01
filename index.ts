49e82d1ba742f66f80e385d570783c5de455d9ff
For_tuna <kachalina.kate96@mail.ru>
2023-07-01 19:30:30 +0300

    any

--- index.ts
+++ index.ts
@@ -6,9 +6,9 @@
 // 10, 0.5, 0.0001, -50, 
 // true, false
 
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = 'John';
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = 'John';
 
 
 // function logBrdMsg (isBirthday: boolean, userName: string, age: number): string {
@@ -19,6 +19,23 @@ const userNameData: string = 'John';
 //         return "Error";
 //     }
 // }
+
+// let salary;
+// salary = 5000; //избегать такую запись
+
+let salary: number; //указание типа сразу
+salary: 5000;
+
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData: "John"}';
+
+const userObj: {
+    //аннотация объекта
+    isBirthdayData: boolean,
+    userNameData: string,
+    ageData: number;
+} = JSON.parse(userData);
+console.log(userObj.smt());
+
 
 const logBrdMsg = (isBirthday: boolean, userName: string, age: number): string  => {
     // void полное игнорирование возвращаемого значения,  если явно не указан - undefined
