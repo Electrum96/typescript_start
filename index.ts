f297002f9a3f7e2d33a177736fdeb8bd5a4b9073
For_tuna <kachalina.kate96@mail.ru>
2023-07-01 18:55:03 +0300

    basic types

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
-const str = 'str';
-console.log(str);
+// let userName: string = "Kate"; //указание нужного типа
+
+// userName = 6;
+
+// '', "", ``
+// 10, 0.5, 0.0001, -50, 
+// true, false
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = 'John';
+
+age = 40;
+
+if (isBirthday) {
+    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`); 
+}
