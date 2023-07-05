88132083ed6f52c30ccc530318161a5c9a10ea45
For_tuna <kachalina.kate96@mail.ru>
2023-07-05 15:13:48 +0300

    symbol, bigint

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
-const test: null = null;
-const test2: any = null;
-const test3: string = null;
-const test4: number = null;
+let id: symbol = Symbol("id");
 
-const test5: undefined = undefined;
-const test6: any = undefined;
-const test7: string = undefined;
-
-let smth; // по умолчанию подставляется any
-
-function getRandomData() {
-    if(Math.random() < 0.5){
-        return null
-    } else {
-        return ' Some data ';
-    }
+const data = {
+    [id]: 1
 }
 
-const data =  getRandomData();
-const trimmedData = data ? data.trim() : null;
+console.log(data[id]);
+
+const num1: bigint = 1n;
+const num2: bigint = 2n;
+
+console.log(num1 + num2);
+
+
+
+
 
 
