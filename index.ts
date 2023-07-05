423a79d6bbd2cd887d024a96b02012ac3fe4a3f4
For_tuna <kachalina.kate96@mail.ru>
2023-07-05 18:58:27 +0300

    objects

--- index.ts
+++ index.ts
@@ -1,18 +1,41 @@
-let id: symbol = Symbol("id");
+const isBirthdayData : boolean = true;
+const userNameData : string = "Kate";
+let ageData : number = 27;
 
-const data = {
-    [id]: 1
+const userData = {
+    isBirthdayData: true,
+    userNameData: "Kate",
+    ageData: 27,
+    messages: {
+        error: "Error"
+    }
+};
+
+const createError = (msg : string) => {
+    throw new Error(msg);
+
+};
+
+function logBrdMsg({
+    isBirthdayData, 
+    ageData, 
+    userNameData, 
+    messages: 
+    {error},
+    }: {
+    isBirthdayData: boolean;
+    userNameData: string;
+    ageData: number;
+    messages: { error: string}
+}) : string {
+    if (isBirthdayData) {
+        return `Congrats ${userNameData.toUpperCase()}, age: ${ageData + 1}`;
+    } else {
+        return createError(error);
+    }
+
 }
 
-console.log(data[id]);
+console.log(logBrdMsg(userData));
 
-const num1: bigint = 1n;
-const num2: bigint = 2n;
-
-console.log(num1 + num2);
-
-
-
-
-
-
+// const smth: never = null; нельзя назначить в never
